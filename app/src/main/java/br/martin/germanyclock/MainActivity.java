fcb808ec89e2f713e6806144a2bf342ac8e5d498
Martin Sch <amrastasu@gmail.com>
2016-10-31 17:00:42 -0200

    Criada a função que irá setar a hora inicial da aplicação.

--- app/src/main/java/br/martin/germanyclock/MainActivity.java
+++ app/src/main/java/br/martin/germanyclock/MainActivity.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     TextView frase;
     TextView hoursTV;
     TextView minutesTV;
+    //TextView antes_depoisTV;
     String[] numberArrayString;
     String[] dezenasArrayString;
 
@@ -35,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         frase = (TextView)findViewById(R.id.descricao);
         hoursTV = (TextView)findViewById(R.id.hoursTV);
         minutesTV = (TextView)findViewById(R.id.minutesTV);
+        //antes_depoisTV = (TextView)findViewById(R.id.antes_depoisTV);
         timePicker.setIs24HourView(true);
 
         final DrawView surface = (DrawView)getWindow().findViewById(R.id.surfaceView4);
@@ -42,12 +46,14 @@ public class MainActivity extends AppCompatActivity {
         numberArrayString = getResources().getStringArray(R.array.numbers);
         dezenasArrayString = getResources().getStringArray(R.array.dezenas);
 
+        configCurrentTime(surface);
+
         timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @Override
             public void onTimeChanged(TimePicker timePicker, final int i, final int i1) {
 
                 Log.d(TAG, "Valor: " + i +":"+i1);
-                surface.setHour(i,i1);
+                surface.setTime(i,i1);
                 Uhr uhr = getGermanize(i,i1, MainActivity.this);
                 hoursTV.setText(uhr.getHour());
                 minutesTV.setText(uhr.getMinute());
@@ -59,6 +65,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Log.d(TAG, "View added");
+    }
+
+    private void configCurrentTime(DrawView surface) {
+
+        Calendar calendar = Calendar.getInstance();
+        surface.setTime(calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE));
+        Uhr uhr = getGermanize(calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), MainActivity.this);
+        hoursTV.setText(uhr.getHour());
+        minutesTV.setText(uhr.getMinute());
+
     }
 
     @Override
@@ -78,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         //String hourString = "";
         Uhr uhr = new Uhr();
 
-        //minute =  11;
+        //minute =  15;
         //hour = 23;
 
         //Log.i("teste", ""+hour);
@@ -133,8 +149,13 @@ public class MainActivity extends AppCompatActivity {
         //antes ou depois
         if((minute > 0 && minute <= 15) || (minute > 30 && minute <= 45)){
             hourString = hourString + " " + "nach";
+            //antes_depoisTV.setText("nach ");
         }else if (minute > 15 && minute < 30 || (minute > 45 && minute < 60)){
             hourString = hourString + " " + "vor";
+            //antes_depoisTV.setText("vor ");
+
+        }else{
+            //antes_depoisTV.setText("");
         }
 
         if(minute > 15 && minute <= 45){
